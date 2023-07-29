0cf16ef7bc43f179e14c4ed51c04fc9c94e5084d
ynn <contact@your-diary.io>
2023-07-30 01:12:08 +0900

    Makes the implementation more practical.

--- README.md
+++ README.md
@@ -2,36 +2,70 @@
 
 An example preprocessor for `mdbook`.
 
-This converts `==[string]==` into `<font color='red'>[string]</font>`, where the color `red` can be customized (see below).
+This provides a simple replace functionality with user-defined regular expressions.
 
 # 2. Configurations
+
+## 2.1 Configurations
 
 Add the configuration below to `book.toml`.
 ```toml
 [preprocessor.ynn]
 command = "<path to executable>"
+patterns = [
+    ["<old>", "<new>"]
+]
 ```
 
-Optionally, you can specify `color` field to change the target color from the default `red` to any color you want.
+- Multiple `["<old>", "<new>"]` can be specified.
+
+- `"<old>"` is a regular expression.
+
+- `"<new>"` is generally a string literal, but `$0`, `$1`, ... can be used to represent capture groups.
+
+- `patterns` itself can be omitted, in which case this preprocessor does nothing.
+
+## 2.2 Example
+
 ```toml
 [preprocessor.ynn]
-command = "<path to executable>"
-color = "blue"
+command = "/Users/user/.cargo/bin/mdbook_preprocessor"
+patterns = [
+    [":warning:", "⚠️"],
+    [":check:", "✅"],
+    ["==(.*?)==", "<font color=Red>$1</font>"],
+]
 ```
 
-# 3. Build
+The last pattern converts `==[string]==` into `<font color=Red>[string]</font>`.
+
+# 3. Installation
+
+## 3.1 From Local
+
+```bash
+$ cargo install --path .
+```
+
+## 3.2 From GitHub
+
+```bash
+$ cargo install --git 'https://github.com/your-diary/mdbook_preprocessor'
+```
+
+# 4. Build
 
 ```bash
 $ cargo build --release
 ```
 
-# 4. Test
+# 5. Test
 
 ```bash
 $ cargo test
 ```
 
-# 5. How To Customize This Project
+# 6. How To Customize This Project
 
 Basically, the only part you may want to edit is the body of the `replace()` function in [`src/replacer.rs`](./src/replacer.rs).
 
@@ -45,11 +79,13 @@ Optionally, you may want to fix or add its tests.
 
 For a more advanced but yet simple example, [the official example preprocessor](https://rust-lang.github.io/mdBook/for_developers/preprocessors.html#hooking-into-mdbook) would help.
 
-# 6. References
+# 7. References
 
 - [*Preprocessors - mdBook Documentation*](https://rust-lang.github.io/mdBook/for_developers/preprocessors.html)
 
 - [*Configuring Preprocessors - mdBook Documentation*](https://rust-lang.github.io/mdBook/format/configuration/preprocessors.html?highlight=command#provide-your-own-command)
+
+- [*TOML: Tom's Obvious Minimal Language*](https://toml.io/en/)
 
 <!-- vim: set spell: -->
 
--- src/replacer.rs
+++ src/replacer.rs
@@ -1,43 +1,76 @@
 use regex::Regex;
 use toml::{Table, Value};
 
+struct Pattern<'a> {
+    old: &'a str,
+    new: &'a str,
+}
+
 //replaces Markdown content
 //`s`: original Markdown
 //`config`: configurations of this preprocessor specified in `book.toml` (`Table` is like `HashMap`)
 pub fn replace(s: &str, config: &Table) -> String {
-    let color = match config.get("color") {
-        Some(Value::String(c)) => c,
-        _ => "Red",
+    let mut patterns = vec![];
+    match config.get("patterns") {
+        Some(Value::Array(l)) => {
+            l.iter().for_each(|l| {
+                let l = l.as_array().unwrap();
+                assert_eq!(2, l.len());
+                let pattern = Pattern {
+                    old: l[0].as_str().unwrap(),
+                    new: l[1].as_str().unwrap(),
+                };
+                patterns.push(pattern);
+            });
+        }
+        _ => return s.to_owned(),
     };
 
-    let re = Regex::new(r#"==(.*?)=="#).unwrap();
-    let s = re
-        .replace_all(s, format!(r#"<font color='{}'>$1</font>"#, color))
-        .to_string();
+    let mut s = s.to_owned();
+    for pattern in patterns {
+        let re = Regex::new(pattern.old).unwrap();
+        s = re.replace_all(&s, pattern.new).to_string();
+    }
 
     s
 }
 
 #[cfg(test)]
 mod test {
+    use toml::value::Array;
+
     use super::*;
 
     #[test]
     fn test01() {
         let config = Table::new();
         assert_eq!(
-            "<font color='Red'>abc=def</font>x<font color='Red'>pq</font>",
-            replace("==abc=def==x==pq==", &config)
+            ":warning: :check: :unknown: ==abc=def==x==pq==",
+            replace(":warning: :check: :unknown: ==abc=def==x==pq==", &config)
         );
     }
 
     #[test]
     fn test02() {
         let mut config = Table::new();
-        config.insert("color".to_owned(), Value::String("blue".to_owned()));
+        let mut patterns = Array::new();
+        patterns.push(Value::Array(Array::from(vec![
+            Value::String(":warning:".to_owned()),
+            Value::String("⚠️".to_owned()),
+        ])));
+        patterns.push(Value::Array(Array::from(vec![
+            Value::String(":check:".to_owned()),
+            Value::String("✅".to_owned()),
+        ])));
+        patterns.push(Value::Array(Array::from(vec![
+            Value::String("==(.*?)==".to_owned()),
+            Value::String("<font color=Red>$1</font>".to_owned()),
+        ])));
+        config.insert("patterns".to_owned(), Value::Array(patterns));
+
         assert_eq!(
-            "<font color='blue'>abc=def</font>x<font color='blue'>pq</font>",
-            replace("==abc=def==x==pq==", &config)
+            "⚠️ ✅ :unknown: <font color=Red>abc=def</font>x<font color=Red>pq</font>",
+            replace(":warning: :check: :unknown: ==abc=def==x==pq==", &config)
         );
     }
 }
